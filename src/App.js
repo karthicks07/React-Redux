face12af367796b802cf3fb576e1c7db7702ed6a
Karthick_S <karthick712004@gmail.com>
2024-03-27 22:13:03 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { buycake } from './redux/cakes/cakeactions';
 function App() {
   return (
     <Provider store={store}>
